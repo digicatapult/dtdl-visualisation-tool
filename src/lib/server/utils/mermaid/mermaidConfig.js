44ae4521c6ae5e950916bd62e56c6e0086634d3e
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2024-09-24 13:59:02 +0100

    installed mermaid-js/layout-elk

--- src/lib/server/utils/mermaid/mermaidConfig.js
+++ src/lib/server/utils/mermaid/mermaidConfig.js
@@ -1,3 +1,18 @@
 import mermaid from '/lib/mermaid/mermaid.esm.mjs'
-let config = { startOnLoad: true, flowchart: { useMaxWidth: false, htmlLabels: true } }
+import elkLayouts from '/lib/mermaid-elk/mermaid-layout-elk.esm.mjs'
+
+mermaid.registerLayoutLoaders(elkLayouts)
+
+let config = { 
+    startOnLoad: true, 
+    flowchart: { 
+        useMaxWidth: false, 
+        htmlLabels: true,
+        rankSpacing: 0
+    },
+    maxTextSize: 99999999,
+    // layout: 'elk.mrtree',
+    
+
+}
 mermaid.initialize(config)
