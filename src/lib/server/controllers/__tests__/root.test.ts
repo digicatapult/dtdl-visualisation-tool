17967a0817f4a0770fb45c3069265a7d722b0b0d
Jonathan Gray <jonmattgray@gmail.com>
2024-09-24 12:51:04 +0100

    revert file

--- src/lib/server/controllers/__tests__/root.test.ts
+++ src/lib/server/controllers/__tests__/root.test.ts
@@ -1,20 +1,22 @@
-import { expect } from 'chai'
 import { describe, it } from 'mocha'
+import { RootController } from '../root'
 import { pino } from 'pino'
 import sinon from 'sinon'
-import { RootController } from '../root'
+import { expect } from 'chai'
+
 
 describe('RootController', async () => {
-  afterEach(() => {
-    sinon.restore()
-  })
 
-  describe('get', () => {
-    it('should return Hello, World', async () => {
-      const mockLogger = pino({ level: 'silent' })
-      const controller = new RootController(mockLogger)
-      const result = await controller.get()
-      expect(result).to.equal('Hello, World')
+    afterEach(() => {
+      sinon.restore()
     })
-  })
+  
+    describe('get', () => {
+      it('should return Hello, World', async () => {
+        const mockLogger = pino({ level: 'silent' })
+        const controller = new RootController(mockLogger)
+        const result = await controller.get()
+        expect(result).to.equal('Hello, World')
+      })
+    })
 })
