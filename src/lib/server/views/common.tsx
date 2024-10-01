b876715600891e0c7fdd5728bc7a89856b1cfb11
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2024-10-01 15:14:11 +0100

    wip

--- src/lib/server/views/common.tsx
+++ src/lib/server/views/common.tsx
@@ -1,15 +1,17 @@
-import { escapeHtml, type PropsWithChildren } from '@kitajs/html'
+/// <reference types="@kitajs/html/all-types.d.ts" />
+import Html from '@kitajs/html'
 
 export const parseError = (): JSX.Element => <p>Ontology Undefined</p>
 
-export const Page = (props: PropsWithChildren<{ title: string }>): JSX.Element => (
+export const Page = (props: Html.PropsWithChildren<{ title: string }>): JSX.Element => (
   <>
     {'<!DOCTYPE html>'}
     <html lang="en">
       <head>
         <script src="/public/scripts/mermaidConfig.js" type="module"></script>
         <link rel="icon" type="image/ico" sizes="48x48" href="/public/images/favicon.ico" />
-        <title>{escapeHtml(props.title)}</title>
+        <link rel="stylesheet" type="text/css" href="/public/styles/main.css" />
+        <title>{Html.escapeHtml(props.title)}</title>
       </head>
       <body>{props.children}</body>
     </html>
