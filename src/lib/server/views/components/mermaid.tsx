03d1d177a80eb142dc72ef4a4b49c426dfaaab37
Matthew Dean <matthew.dean@digicatapult.org.uk>
2024-10-22 17:10:30 +0100

    Search for entities to display (#35)
    * Search for entities to display
    
    * Updating version to 0.0.31
    
    * Updating version to 0.0.32
    
    * filter unit tests
    
    * controller unit tests
    
    * fix test
    
    * Updating version to 0.0.32
    
    * Add not found when no graph is present and change buttons to a dropdown
    
    * push url
    
    ---------
    
    Co-authored-by: dc-autobot[bot] <181364585+dc-autobot[bot]@users.noreply.github.com>

--- src/lib/server/views/components/mermaid.tsx
+++ src/lib/server/views/components/mermaid.tsx
@@ -4,13 +4,20 @@ import { singleton } from 'tsyringe'
 import { type Layout, layoutEntries } from '../../models/mermaidLayouts.js'
 import { Page } from '../common.js'
 
+const commonUpdateAttrs = {
+  'hx-target': '#graphMarkdown',
+  'hx-get': '/update-layout',
+  'hx-swap': 'outerHTML',
+  'hx-include': '#layout-buttons',
+}
+
 @singleton()
 export default class MermaidTemplates {
   constructor() {}
 
-  public MermaidRoot = ({ graph, layout }: { graph: string; layout: Layout }) => (
+  public MermaidRoot = ({ graph, search, layout }: { graph: string | null; search?: string; layout: Layout }) => (
     <Page title={'Mermaid Ontology visualiser'}>
-      <this.layoutForm layout={layout} />
+      <this.layoutForm layout={layout} search={search} />
       <this.mermaidTarget target="mermaid-output" />
       <this.mermaidMarkdown graph={graph} />
       <div id="navigation-panel">
@@ -21,43 +28,52 @@ export default class MermaidTemplates {
     </Page>
   )
 
-  public mermaidMarkdown = ({ graph, layout }: { graph: string; layout?: Layout }) => {
+  public mermaidMarkdown = ({ graph, layout }: { graph: string | null; layout?: Layout }) => {
     const attributes = layout
       ? {
           'hx-on::after-settle': `globalThis.renderLayoutChange('mermaid-output', 'graphMarkdown', '${layout}')`,
         }
       : {
-          'hx-get': '/update-layout',
-          'hx-swap': 'outerHTML',
+          ...commonUpdateAttrs,
+
           'hx-trigger': 'load',
           'hx-on::after-settle': `globalThis.renderMermaid('mermaid-output', 'graphMarkdown')`,
         }
     return (
       <div id="graphMarkdown" style="display: none" {...attributes}>
-        {escapeHtml(graph)}
+        {escapeHtml(graph ?? '')}
       </div>
     )
   }
 
   private mermaidTarget = ({ target }: { target: string }) => <div id={target} class="mermaid"></div>
 
-  public layoutForm = ({ layout, swapOutOfBand }: { layout: Layout; swapOutOfBand?: boolean }) => {
-    const attributes = {
-      'hx-target': '#graphMarkdown',
-      'hx-get': '/update-layout',
-      'hx-swap': 'outerHTML',
-    }
-    if (swapOutOfBand) {
-      attributes['hx-swap-oob'] = 'true'
-    }
-
+  public layoutForm = ({
+    search,
+    layout,
+    swapOutOfBand,
+  }: {
+    search?: string
+    layout: Layout
+    swapOutOfBand?: boolean
+  }) => {
     return (
-      <form id="layout-buttons" class="button-group" {...attributes}>
-        {layoutEntries.map((entry) => (
-          <button name="layout" value={entry} class={entry === layout ? 'highlighted' : ''}>
-            {escapeHtml(entry)}
-          </button>
-        ))}
+      <form id="layout-buttons" class="button-group" hx-swap-oob={swapOutOfBand ? 'true' : undefined}>
+        <input
+          id="search"
+          name="search"
+          type="search"
+          value={escapeHtml(search || '')}
+          hx-trigger="input changed delay:500ms, search"
+          {...commonUpdateAttrs}
+        />
+        <select id="layout" name="layout" hx-trigger="input changed" {...commonUpdateAttrs}>
+          {layoutEntries.map((entry) => (
+            <option value={entry} selected={entry === layout}>
+              {escapeHtml(entry)}
+            </option>
+          ))}
+        </select>
       </form>
     )
   }
