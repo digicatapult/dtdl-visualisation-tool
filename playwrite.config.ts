d9fdacf64717cb64f6fe5e271b519e8564446152
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2024-11-14 09:44:11 +0000

    lint

--- playwrite.config.ts
+++ playwrite.config.ts
@@ -1,20 +1,20 @@
 import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
-    testDir: './test/e2e/',
-    testMatch: '**/*.spec.ts',
-    projects: [
-        {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
-        },
-        {
-            name: 'firefox',
-            use: { ...devices['Desktop Firefox'] },
-        },
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
-    ],
+  testDir: './test/e2e',
+  testMatch: './**/*.spec.ts',
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
 })
--- test/e2e/xss.spec.ts
+++ test/e2e/xss.spec.ts
@@ -1,13 +1,12 @@
 import { expect, test } from '@playwright/test'
 
 test.describe('homepage redirect', () => {
-
   test(`url is as expected'`, async ({ page }) => {
     const baseUrl = 'http://127.0.0.1:3000'
     const expectedUrl = `${baseUrl}/?search=&highlightNodeId=&lastSearch=&diagramType=flowchart&layout=elk&output=svg`
 
     await page.goto(baseUrl, {
-      waitUntil: 'networkidle'
+      waitUntil: 'networkidle',
     })
 
     const url = page.url()
@@ -15,25 +14,25 @@ test.describe('homepage redirect', () => {
   })
 
   const payloads = [
-    "<script>alert('XSS')</script>",
-    "<img src='x' onerror='alert(\"XSS\")'>",
-    "<div onclick='alert(\"XSS\")'>Click me</div>",
-  ];
+    `<script>alert('XSS')</script>`,
+    `<img src='x' onerror='alert(\"XSS\")'>`,
+    `<div onclick=\'alert("XSS")\'>Click me</div>`,
+  ]
   payloads.forEach((payload) => {
     test(`xss attack with payload ${payload}`, async ({ page }) => {
       const baseUrl = 'http://127.0.0.1:3000'
       await page.goto(baseUrl, {
-        waitUntil: 'networkidle'
+        waitUntil: 'networkidle',
       })
 
       await page.fill('#search', payload)
       await page.waitForLoadState('networkidle')
 
       const alertMessage = await page.evaluate(() => {
-        return new Promise(resolve => {
-          window.alert = resolve;
-        });
-      });
+        return new Promise((resolve) => {
+          window.alert = resolve
+        })
+      })
       expect(alertMessage).toBeUndefined()
     })
   })
