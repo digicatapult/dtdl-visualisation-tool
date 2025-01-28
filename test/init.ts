b2122a6cf05ecd93d6efe3234d761f1ada613ce5
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-01-28 10:14:33 +0000

    lint

--- test/init.ts
+++ test/init.ts
@@ -1,15 +1,15 @@
 import 'reflect-metadata'
 
-import { StartedTestContainer } from 'testcontainers'
-import { bringUpDatabaseContainer, bringUpVisualisationContainer, stopAllContainers } from './testcontainers/testContainersSetup.js'
-
-let databaseContainer: StartedTestContainer
-let visualisationContainer: StartedTestContainer
+import {
+  bringUpDatabaseContainer,
+  bringUpVisualisationContainer,
+  stopAllContainers,
+} from './testcontainers/testContainersSetup.js'
 
 before(async function () {
   this.timeout(420000)
-  databaseContainer = await bringUpDatabaseContainer()
-  visualisationContainer = await bringUpVisualisationContainer()
+  await bringUpDatabaseContainer()
+  await bringUpVisualisationContainer()
 })
 
 after(async function () {
