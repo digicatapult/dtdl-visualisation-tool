4823881292ef09b984c6eb8d0dd283a65ba74b02
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-01-28 09:36:06 +0000

    lint

--- test/e2e/uploadOntology.spec.ts
+++ test/e2e/uploadOntology.spec.ts
@@ -10,20 +10,20 @@ test.describe('Upload ontology from local drive', () => {
   test('Should error and show toast to user', async ({ page }) => {
     // Set viewport and navigate to the page, smaller viewports hide UI elements
     await page.setViewportSize({ width: 1920, height: 1080 })
-      await Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-          page.goto('./')
-      ])
+    await Promise.all([
+      page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+      page.goto('./'),
+    ])
 
     expect(await page.isVisible("text='Upload Ontology'")).toBe(true)
 
-      // Upload ontology and wait for file to load dtdl
+    // Upload ontology and wait for file to load dtdl
     const filePath = path.join(__dirname, '../../src/lib/server/controllers/__tests__/error.zip')
-      await Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/upload') && resp.status() === 400),
-          page.getByLabel('Upload Ontology').setInputFiles(filePath)
-      ])
-      await page.waitForTimeout(5000)
+    await Promise.all([
+      page.waitForResponse((resp) => resp.url().includes('/upload') && resp.status() === 400),
+      page.getByLabel('Upload Ontology').setInputFiles(filePath),
+    ])
+    await page.waitForTimeout(5000)
 
     expect(await page.isVisible("text='Failed to parse DTDL model'")).toBe(true)
   })
@@ -32,34 +32,34 @@ test.describe('Upload ontology from local drive', () => {
   }) => {
     // Set viewport and navigate to the page, smaller viewports hide UI elements
     await page.setViewportSize({ width: 1920, height: 1080 })
-      await Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-          page.goto('./')
-      ])
+    await Promise.all([
+      page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+      page.goto('./'),
+    ])
 
     expect(await page.isVisible("text='Upload Ontology'")).toBe(true)
 
-      // Upload ontology and wait for file to load dtdl
+    // Upload ontology and wait for file to load dtdl
     const filePath = path.join(__dirname, '../../src/lib/server/controllers/__tests__/simple.zip')
-      await Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-          page.getByLabel('Upload Ontology').setInputFiles(filePath)
-      ])
+    await Promise.all([
+      page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+      page.getByLabel('Upload Ontology').setInputFiles(filePath),
+    ])
     await page.waitForTimeout(5000)
     expect(await page.isVisible("text='dtmi:com:example;1'")).toBe(true)
 
-      // Render root page and test if default dtdl has loaded
-      await Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-          page.goto('./')
-      ])
+    // Render root page and test if default dtdl has loaded
+    await Promise.all([
+      page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+      page.goto('./'),
+    ])
     expect(await page.isVisible("text='ConnectivityNodeContainer'")).toBe(true)
 
-      // Check classDiagram functionality
-      await Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-          await page.getByLabel('Diagram Type').selectOption('classDiagram')
-      ])
+    // Check classDiagram functionality
+    await Promise.all([
+      page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+      await page.getByLabel('Diagram Type').selectOption('classDiagram'),
+    ])
     await page.waitForTimeout(5000)
     expect(await page.isVisible("text='mRID'")).toBe(true)
 
