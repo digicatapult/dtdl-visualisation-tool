267fd4d01d77816d99babaee900031f56245b08e
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-01-28 09:33:45 +0000

    tests passing locally

--- test/e2e/uploadOntology.spec.ts
+++ test/e2e/uploadOntology.spec.ts
@@ -10,15 +10,20 @@ test.describe('Upload ontology from local drive', () => {
   test('Should error and show toast to user', async ({ page }) => {
     // Set viewport and navigate to the page, smaller viewports hide UI elements
     await page.setViewportSize({ width: 1920, height: 1080 })
-    await page.goto('./?layout=elk&diagramType=flowchart&search=Node')
+      await Promise.all([
+          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+          page.goto('./')
+      ])
 
     expect(await page.isVisible("text='Upload Ontology'")).toBe(true)
 
-    // click upload ontology and wait for file to load dtdl
-    await page.click('id=upload-button')
+      // Upload ontology and wait for file to load dtdl
     const filePath = path.join(__dirname, '../../src/lib/server/controllers/__tests__/error.zip')
-    await page.setInputFiles('input[type="file"]', filePath)
-    await Promise.all([await page.waitForResponse((resp) => resp.url().includes('/upload') && resp.status() === 400)])
+      await Promise.all([
+          page.waitForResponse((resp) => resp.url().includes('/upload') && resp.status() === 400),
+          page.getByLabel('Upload Ontology').setInputFiles(filePath)
+      ])
+      await page.waitForTimeout(5000)
 
     expect(await page.isVisible("text='Failed to parse DTDL model'")).toBe(true)
   })
@@ -27,25 +32,34 @@ test.describe('Upload ontology from local drive', () => {
   }) => {
     // Set viewport and navigate to the page, smaller viewports hide UI elements
     await page.setViewportSize({ width: 1920, height: 1080 })
-    await page.goto('./?layout=elk&diagramType=flowchart&search=Node')
+      await Promise.all([
+          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+          page.goto('./')
+      ])
 
     expect(await page.isVisible("text='Upload Ontology'")).toBe(true)
 
-    // click upload ontology and wait for file to load dtdl
+      // Upload ontology and wait for file to load dtdl
     const filePath = path.join(__dirname, '../../src/lib/server/controllers/__tests__/simple.zip')
-    await page.getByLabel('Upload Ontology').setInputFiles(filePath)
-    await Promise.all([page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200)])
+      await Promise.all([
+          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+          page.getByLabel('Upload Ontology').setInputFiles(filePath)
+      ])
     await page.waitForTimeout(5000)
     expect(await page.isVisible("text='dtmi:com:example;1'")).toBe(true)
 
-    await page.goto('./')
-    await page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200)
-    await page.waitForTimeout(5000)
+      // Render root page and test if default dtdl has loaded
+      await Promise.all([
+          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+          page.goto('./')
+      ])
     expect(await page.isVisible("text='ConnectivityNodeContainer'")).toBe(true)
 
-    // check classDiagram functionality
-    await page.getByLabel('Diagram Type').selectOption('classDiagram')
-    await Promise.all([page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200)])
+      // Check classDiagram functionality
+      await Promise.all([
+          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+          await page.getByLabel('Diagram Type').selectOption('classDiagram')
+      ])
     await page.waitForTimeout(5000)
     expect(await page.isVisible("text='mRID'")).toBe(true)
 
