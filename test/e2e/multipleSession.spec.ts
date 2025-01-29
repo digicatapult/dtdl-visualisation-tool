ad2f250f3174fd78303754972050ad4cd4d5cd60
Jonathan Gray <jonmattgray@gmail.com>
2025-01-29 18:16:19 +0000

    match await playwright style

--- test/e2e/multipleSession.spec.ts
+++ test/e2e/multipleSession.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test'
+import { expect, Page, test } from '@playwright/test'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-test.describe('Upload ontology from local drive', () => {
+test.describe('multiple sessions', () => {
   test('render two simultaneous sessions correctly', async ({ browser }) => {
     const context = await browser.newContext({ viewport: { width: 1920, height: 1080 } })
     const pageOne = await context.newPage()
@@ -15,29 +15,23 @@ test.describe('Upload ontology from local drive', () => {
 
     // load both pages
     await Promise.all(
-      pages.map((page) =>
-        Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-          page.goto('./'),
-        ])
-      )
+      pages.map(async (page) => {
+        await waitForUpdateLayout(page, () => page.goto('./'))
+      })
     )
 
     // Upload ontology to second page
     const filePath = path.join(__dirname, '../../src/lib/server/controllers/__tests__/simple.zip')
-    await Promise.all([
-      pageTwo.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-      pageTwo.locator('#toolbar').getByLabel('Upload Ontology').setInputFiles(filePath),
-    ])
+
+    await waitForUpdateLayout(pageTwo, () =>
+      pageTwo.locator('#toolbar').getByLabel('Upload Ontology').setInputFiles(filePath)
+    )
 
     // change to class diagram type on both pages
     await Promise.all(
-      pages.map((page) =>
-        Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-          page.getByLabel('Diagram Type').selectOption('classDiagram'),
-        ])
-      )
+      pages.map(async (page) => {
+        await waitForUpdateLayout(page, () => page.getByLabel('Diagram Type').selectOption('classDiagram'))
+      })
     )
 
     // check both pages are still showing their correct model
@@ -46,19 +40,21 @@ test.describe('Upload ontology from local drive', () => {
 
     // search for a node on both pages
     await pageOne.focus('#search')
-    await Promise.all([
-      pageOne.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-      pageOne.fill('#search', 'Container'),
-    ])
+    await waitForUpdateLayout(pageOne, () => pageOne.fill('#search', 'Container'))
 
     await pageTwo.focus('#search')
-    await Promise.all([
-      pageTwo.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-      pageTwo.fill('#search', 'example'),
-    ])
+    await waitForUpdateLayout(pageTwo, () => pageTwo.fill('#search', 'example'))
 
     // check both pages are still showing their correct model
     await expect(pageOne.locator('#mermaid-output').getByText('ConnectivityNode', { exact: true })).toBeVisible()
     await expect(pageTwo.locator('#mermaid-output').getByText('dtmi:com:example;1')).toBeVisible()
   })
 })
+
+const waitForUpdateLayout = async <T>(page: Page, action: () => Promise<T>) => {
+  const updateLayoutResponse = page.waitForResponse(
+    (resp) => resp.url().includes('/update-layout') && resp.status() === 200
+  )
+  await action()
+  await updateLayoutResponse
+}
