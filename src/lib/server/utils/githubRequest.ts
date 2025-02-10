cd9492ecb7a4c67ac2fcc5df523e43ba9ee0a947
Jonathan Gray <jonmattgray@gmail.com>
2025-02-10 08:37:58 +0000

    separate github css, safe url params, move fns

--- src/lib/server/utils/githubRequest.ts
+++ src/lib/server/utils/githubRequest.ts
@@ -4,6 +4,7 @@ import { Env } from '../env.js'
 import { GithubReqError } from '../errors.js'
 
 import { Logger, type ILogger } from '../logger.js'
+import { OAuthToken } from '../models/github.js'
 
 const env = container.resolve(Env)
 
@@ -58,6 +59,29 @@ export class GithubRequest {
       throw new GithubReqError('Attempted to get contents of a file rather than directory')
 
     return response.data
+  }
+
+  getAccessToken = async (code: string): Promise<OAuthToken> => {
+    const url = `https://github.com/login/oauth/access_token`
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        client_id: env.get('GH_CLIENT_ID'),
+        client_secret: env.get('GH_CLIENT_SECRET'),
+        code,
+      }),
+    })
+    const json = await response.json()
+
+    if (!response.ok || !json.access_token) {
+      throw new GithubReqError(`Unexpected error fetching GitHub token`)
+    }
+
+    return json as OAuthToken
   }
 
   private async requestWrapper<T>(request: () => Promise<T>): Promise<T> {
