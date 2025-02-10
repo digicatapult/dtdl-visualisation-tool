b4e2dd6d55bdd76a02860b9e4945770e07ddb209
Matthew Dean <matthew.dean@digicatapult.org.uk>
2025-02-10 15:23:58 +0000

    Improve render performance

--- src/lib/server/utils/mermaid/generator.ts
+++ src/lib/server/utils/mermaid/generator.ts
@@ -1,6 +1,9 @@
+import path from 'node:path'
+import url from 'node:url'
+
 import { DtdlObjectModel } from '@digicatapult/dtdl-parser'
-import { renderMermaid, type ParseMDDOptions } from '@mermaid-js/mermaid-cli'
-import puppeteer, { Browser } from 'puppeteer'
+import type { MermaidConfig } from 'mermaid'
+import puppeteer, { Browser, Page } from 'puppeteer'
 import { inject, singleton } from 'tsyringe'
 
 import { Logger, type ILogger } from '../../logger.js'
@@ -11,64 +14,119 @@ import ClassDiagram from './classDiagram.js'
 import { IDiagram } from './diagramInterface.js'
 import Flowchart from './flowchart.js'
 
+const mermaidJsPath = path.resolve(
+  path.dirname(url.fileURLToPath(import.meta.resolve('mermaid', import.meta.url))),
+  'mermaid.js'
+)
+const mermaidHTMLPath = path.resolve(
+  path.dirname(url.fileURLToPath(import.meta.resolve('@mermaid-js/mermaid-cli', import.meta.url))),
+  '..',
+  'dist',
+  'index.html'
+)
+
 @singleton()
 export class SvgGenerator {
-  public browser: Promise<Browser>
-  constructor(@inject(Logger) private logger: ILogger) {
-    this.browser = puppeteer.launch({})
-  }
-
-  mermaidMarkdownByDiagramType: {
+  private init: Promise<{ browser: Browser; page: Page }>
+  private mermaidMarkdownByDiagramType: {
     [k in DiagramType]: IDiagram<k>
   } = {
     flowchart: new Flowchart(),
     classDiagram: new ClassDiagram(),
   }
 
+  constructor(@inject(Logger) private logger: ILogger) {
+    this.init = this.initialise()
+  }
+
   async run(
     dtdlObject: DtdlObjectModel,
     diagramType: DiagramType,
     layout: Layout,
-    options: ParseMDDOptions = {},
     isRetry: boolean = false
   ): Promise<MermaidSvgRender | PlainTextRender> {
     try {
-      //  Mermaid config
-      const parseMDDOptions: ParseMDDOptions = {
-        ...options,
-        svgId: 'mermaid-svg',
-        mermaidConfig: {
-          flowchart: {
-            useMaxWidth: false,
-            htmlLabels: false,
-          },
-          maxTextSize: 99999999,
-          securityLevel: 'strict',
-          maxEdges: 99999999,
-          layout,
-        },
-      }
-
       const graph = this.mermaidMarkdownByDiagramType[diagramType].generateMarkdown(dtdlObject, ' TD')
       if (!graph) {
         return new PlainTextRender('No graph')
       }
 
-      const { data } = await renderMermaid(await this.browser, graph, 'svg', parseMDDOptions)
-      return new MermaidSvgRender(Buffer.from(data))
+      const data = await this.render(layout, graph)
+      return new MermaidSvgRender(data)
     } catch (err) {
       this.logger.warn('Something went wrong rendering mermaid layout', err)
       if (!isRetry) {
         this.logger.info('Attempting to relaunch puppeteer')
 
-        const oldBrowser = await this.browser
-        await oldBrowser.close()
-
-        this.browser = puppeteer.launch({})
-        return this.run(dtdlObject, diagramType, layout, options, true)
+        await this.tryCloseBrowser()
+        this.init = this.initialise()
+        return this.run(dtdlObject, diagramType, layout, true)
       }
       this.logger.error('Something went wrong rendering mermaid layout', err)
       throw err
     }
+  }
+
+  private async tryCloseBrowser() {
+    try {
+      const { browser } = await this.init
+      await browser.close()
+    } catch (err) {
+      this.logger.warn('Failed to close browser %s', err instanceof Error ? err.message : err)
+    }
+  }
+
+  private async initialise() {
+    const browser = await puppeteer.launch({})
+    const page = await browser.newPage()
+
+    page.on('console', (msg) => {
+      this.logger.warn(msg.text())
+    })
+
+    await page.goto(url.pathToFileURL(mermaidHTMLPath).href)
+    await page.addScriptTag({ path: mermaidJsPath })
+    await page.evaluate(async () => {
+      await Promise.all(Array.from(document.fonts, (font) => font.load()))
+      const { mermaid, elkLayouts } = globalThis as any
+      mermaid.registerLayoutLoaders(elkLayouts)
+    })
+
+    return { browser, page }
+  }
+
+  private async render(layout: Layout, definition: string) {
+    const svgId = 'mermaid-svg'
+    const mermaidConfig: MermaidConfig = {
+      flowchart: {
+        useMaxWidth: false,
+        htmlLabels: false,
+      },
+      maxTextSize: 99999999,
+      securityLevel: 'strict',
+      maxEdges: 99999999,
+      layout,
+    }
+
+    const { page } = await this.init
+    const svg = await page.$eval(
+      '#container',
+      async (container, mermaidConfig, definition, svgId) => {
+        const { mermaid } = globalThis as any
+
+        mermaid.initialize({ startOnLoad: false, ...mermaidConfig })
+        const { svg: svgText } = await mermaid.render(svgId, definition, container)
+        container.innerHTML = svgText
+
+        const svg = container.getElementsByTagName?.('svg')?.[0]
+        const xmlSerializer = new XMLSerializer()
+        return xmlSerializer.serializeToString(svg)
+      },
+      mermaidConfig,
+      definition,
+      svgId
+    )
+
+    return Buffer.from(svg)
   }
 }
