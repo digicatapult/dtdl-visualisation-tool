a6c94186a965429fa6b20bc8bf0052e4cde94cbb
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-03-04 07:18:32 +0000

    lint

--- test/e2e/editMode.spec.ts
+++ test/e2e/editMode.spec.ts
@@ -8,13 +8,13 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 test.describe('Open Ontology from recently visited', () => {
-    test('open ontology that can be edited', async ({ page }) => {
-        await page.setViewportSize({ width: 1920, height: 1080 })
-        await waitForUpdateLayout(page, () => page.goto('./'))
-        await expect(page.locator('#toolbar').getByText('Open Ontology')).toBeVisible()
+  test('open ontology that can be edited', async ({ page }) => {
+    await page.setViewportSize({ width: 1920, height: 1080 })
+    await waitForUpdateLayout(page, () => page.goto('./'))
+    await expect(page.locator('#toolbar').getByText('Open Ontology')).toBeVisible()
 
-        await waitForSuccessResponse(page, () => page.locator('#open-button').click(), '/open')
-        await expect(page.locator('#main-view').getByText('Viewed Today at ')).toBeVisible()
-    })
-    test('open ontology than can be edited and check edit function works', async ({ page }) => { })
+    await waitForSuccessResponse(page, () => page.locator('#open-button').click(), '/open')
+    await expect(page.locator('#main-view').getByText('Viewed Today at ')).toBeVisible()
+  })
+  test('open ontology than can be edited and check edit function works', async ({ page }) => {})
 })
