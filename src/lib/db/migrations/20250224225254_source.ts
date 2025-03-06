7764a8719f07406c521f0da2f7da7439d63ec320
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-03-06 12:36:07 +0000

    Update 20250224225254_source.ts

--- src/lib/db/migrations/20250224225254_source.ts
+++ src/lib/db/migrations/20250224225254_source.ts
@@ -3,7 +3,9 @@ import { fileSource } from '../../server/models/openTypes.js'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('model', (table) => {
-    table.enum('source', [...fileSource]).nullable()
+    table.enum('source', fileSource).nullable()
+    table.text('owner').nullable()
+    table.text('repo').nullable()
   })
 }
 
