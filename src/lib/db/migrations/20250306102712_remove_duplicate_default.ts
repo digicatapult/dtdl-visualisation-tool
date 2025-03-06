3eef7a7065f293c8047ddc99dfa467d5f4492114
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-03-06 12:02:02 +0000

    migration to delete all defaults except latest

--- src/lib/db/migrations/20250306102712_remove_duplicate_default.ts
+++ src/lib/db/migrations/20250306102712_remove_duplicate_default.ts
@@ -1,11 +1,10 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-    
+  await knex('model')
+    .where('name', 'default')
+    .andWhere('id', '!=', () => {
+      knex.select('id').from('model').where('name', 'default').orderBy('created_at', 'desc').limit(1)
+    })
+    .del()
 }
-
-
-export async function down(knex: Knex): Promise<void> {
-}
-
