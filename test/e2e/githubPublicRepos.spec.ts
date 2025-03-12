d3e36415f8db465390ea09227b8e721984b99a1f
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-03-12 13:45:58 +0000

    No need to go to root page

--- test/e2e/githubPublicRepos.spec.ts
+++ test/e2e/githubPublicRepos.spec.ts
@@ -6,8 +6,7 @@ test.describe('Upload ontology from GitHub via OAuth', () => {
   test('Success path for uploading ontology from private Github repo + from public Github repo', async ({ page }) => {
     // Set viewport and navigate to the page, smaller viewports hide UI elements
     await page.setViewportSize({ width: 1920, height: 1080 })
-    await waitForUpdateLayout(page, () => page.goto('./'))
-    await waitForSuccessResponse(page, () => page.locator('#open-button').click(), '/open')
+    await waitForUpdateLayout(page, () => page.goto('./open'))
     await waitForSuccessResponse(page, () => page.locator('#main-view').getByText('Upload New File').click(), '/menu')
     await waitForSuccessResponse(page, () => page.locator('#main-view').getByText('GitHub').click(), '/repos')
 
