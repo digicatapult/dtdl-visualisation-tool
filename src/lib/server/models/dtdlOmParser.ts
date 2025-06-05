1e2c001257a478681d24130bb9aa5949ce2fc334
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-06-05 19:35:01 +0100

    added in comment for breaking type safety

--- src/lib/server/models/dtdlOmParser.ts
+++ src/lib/server/models/dtdlOmParser.ts
@@ -7,5 +7,5 @@ const entityParser = z
     Id: z.string(),
   })
   .passthrough()
-
+// This is inherently breaking the type safety of this parser, should be fixed by NIDT-155 https://digicatapult.atlassian.net/browse/NIDT-155
 export const dtdlObjectModelParser = z.record(entityParser).transform((data) => data as unknown as DtdlObjectModel)
