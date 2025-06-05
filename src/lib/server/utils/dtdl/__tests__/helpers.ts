91a911ad9bd3991e17e164de0de44c422e6d16b1
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-06-05 18:32:36 +0100

    tests for cache

---
+++ src/lib/server/utils/dtdl/__tests__/helpers.ts
@@ -0,0 +1,46 @@
+import { createHash } from 'crypto'
+import sinon from 'sinon'
+import { type ICache } from '../../cache'
+
+export function createHashKey(files: { contents: string }[]): { allContents: string; hashKey: string } {
+  const allContents = `[${files.map((f) => f.contents).join(',')}]`
+  return { allContents, hashKey: createHash('sha256').update(allContents).digest('base64') }
+}
+
+export function createMockCache(
+  hashKey: string,
+  parsedValue: unknown,
+  hasCache: boolean
+): {
+  mockCache: ICache
+  hasStub: sinon.SinonStub
+  getStub: sinon.SinonStub
+  setSpy: sinon.SinonSpy
+  clearStub: sinon.SinonStub
+  sizeStub: sinon.SinonStub
+} {
+  const hasStub = sinon.stub().returns(hasCache)
+  const getStub = sinon.stub().callsFake((key, _parser) => {
+    if (hashKey === key) return parsedValue
+    return undefined
+  })
+
+  const setSpy = sinon.spy()
+  const clearStub = sinon.stub()
+  const sizeStub = sinon.stub().returns(0)
+
+  return {
+    mockCache: {
+      has: hasStub,
+      get: getStub,
+      set: setSpy,
+      clear: clearStub,
+      size: sizeStub,
+    },
+    hasStub,
+    getStub,
+    setSpy,
+    clearStub,
+    sizeStub,
+  }
+}
