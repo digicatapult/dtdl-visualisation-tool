e45f046a6a35b2fd334ce4e690db8896a5c2aefa
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-06-11 15:51:50 +0100

    lint

--- test/e2e/oversizedOntology.spec.ts
+++ test/e2e/oversizedOntology.spec.ts
@@ -8,25 +8,25 @@ test.describe('Testing against large ontologies', () => {
     // Set viewport and navigate to the page, smaller viewports hide UI elements
     await page.setViewportSize({ width: 1920, height: 1080 })
     await page.goto(`${limitedOntologySizeHost}`)
-      // Load default ontology which is larger than the max size
-      await expect(
-          page.locator('#mermaid-output').getByText('DtdlObject size exceeds maximum allowed size', { exact: true })
-      ).toBeVisible()
-      // Filter for a smaller ontology from search
-      await page.focus('#search')
-      await Promise.all([
-          page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
-          page.fill('#search', 'voltageLevel'),
-      ])
+    // Load default ontology which is larger than the max size
+    await expect(
+      page.locator('#mermaid-output').getByText('DtdlObject size exceeds maximum allowed size', { exact: true })
+    ).toBeVisible()
+    // Filter for a smaller ontology from search
+    await page.focus('#search')
+    await Promise.all([
+      page.waitForResponse((resp) => resp.url().includes('/update-layout') && resp.status() === 200),
+      page.fill('#search', 'voltageLevel'),
+    ])
 
-      await expect(
-          page.locator('#mermaid-output').locator('[id*=VoltageLevel]').getByText('VoltageLevel', { exact: true })
-      ).toBeVisible()
-      // expand
-      const expand = page.locator('#mermaid-output').locator('[id*=Substation]').getByText('+', { exact: true })
-      await waitForUpdateLayout(page, () => directlyClickElement(expand))
+    await expect(
+      page.locator('#mermaid-output').locator('[id*=VoltageLevel]').getByText('VoltageLevel', { exact: true })
+    ).toBeVisible()
+    // expand
+    const expand = page.locator('#mermaid-output').locator('[id*=Substation]').getByText('+', { exact: true })
+    await waitForUpdateLayout(page, () => directlyClickElement(expand))
 
-      // Dtdl becomes oversized again
+    // Dtdl becomes oversized again
     await expect(
       page.locator('#mermaid-output').getByText('DtdlObject size exceeds maximum allowed size', { exact: true })
     ).toBeVisible()
