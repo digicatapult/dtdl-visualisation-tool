0a9016bc05413f28ef7862c19dfba26a6a24b2e5
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-06-12 16:23:38 +0100

    fixed broken integration

--- test/integration/init.ts
+++ test/integration/init.ts
@@ -2,14 +2,21 @@ import 'reflect-metadata'
 
 import {
   bringUpDatabaseContainer,
-  bringUpVisualisationContainer,
+  buildVisualisationImage,
+  startVisualisationContainer,
   stopAllContainers,
 } from '../testcontainers/testContainersSetup.js'
 
 before(async function () {
   this.timeout(420000)
   await bringUpDatabaseContainer()
-  await bringUpVisualisationContainer()
+  await buildVisualisationImage()
+  await startVisualisationContainer({
+    containerName: 'dtdl-visualiser',
+    hostPort: 3000,
+    containerPort: 3000,
+    cookieSessionKeys: 'secret',
+  })
 })
 
 after(async function () {
