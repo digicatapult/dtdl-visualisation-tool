e40607970849419ede7d1669a2efeb54e4f5c407
Jonathan Gray <jonmattgray@gmail.com>
2025-06-20 09:59:31 +0100

    make test a dep

--- test/e2e/githubPrivateRepo.spec.ts
+++ test/e2e/githubPrivateRepo.spec.ts
@@ -4,7 +4,7 @@ import { waitForSuccessResponse } from './helpers/waitForHelpers'
 const ghAppName = process.env.GH_APP_NAME
 
 test.describe('Private GitHub repos', () => {
-  test('can be authorised via GitHub App install', async ({ page, context }) => {
+  test('authorise all private repos via GitHub App install', async ({ page, context }) => {
     await page.setViewportSize({ width: 1920, height: 1080 })
     await page.goto('./open')
     await expect(page.locator('#main-view').getByTitle('Upload New Ontology')).toBeVisible()
@@ -31,14 +31,17 @@ test.describe('Private GitHub repos', () => {
     if ((await install.count()) > 0) {
       await install.click()
       await githubAppPage.locator('a[href*="target_type=User"]').click()
-      await githubAppPage.getByText('Install & Authorize', { exact: true }).click()
+      await githubAppPage.locator('button:has-text("Install")').click()
 
-      // assert redirect
-      await expect(githubAppPage).toHaveURL(`http://localhost:3000/open`)
-      await expect(githubAppPage.locator('.github-list >> text=/\\/private/')).toBeVisible()
-    } else {
-      // assert private repo is in the list
-      await expect(page.locator('.github-list >> text=/\\/private/')).toBeVisible()
+      // reload original tab
+      await page.reload()
+      await waitForSuccessResponse(
+        page,
+        () => page.locator('#main-view').getByTitle('Upload New Ontology').click(),
+        '/menu'
+      )
+      await waitForSuccessResponse(page, () => page.locator('#main-view').getByText('GitHub').click(), '/github/picker')
     }
+    await expect(page.locator('.github-list >> text=/\\/private/')).toBeVisible()
   })
 })
