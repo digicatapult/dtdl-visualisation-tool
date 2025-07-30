38684d58adfff0010cb87eebc872fbce96d2aa6a
syedhasandigi <82388499+syedhasandigi@users.noreply.github.com>
2025-07-30 14:32:24 +0100

    looseObject

--- src/lib/server/models/dtdlOmParser.ts
+++ src/lib/server/models/dtdlOmParser.ts
@@ -1,12 +1,10 @@
 import { DtdlObjectModel } from '@digicatapult/dtdl-parser'
 import { z } from 'zod'
 
-const entityParser = z
-  .object({
-    EntityKind: z.string(),
-    Id: z.string(),
-  })
-  .loose()
+const entityParser = z.looseObject({
+  EntityKind: z.string(),
+  Id: z.string(),
+})
 // This is inherently breaking the type safety of this parser, should be fixed by NIDT-155 https://digicatapult.atlassian.net/browse/NIDT-155
 export const dtdlObjectModelParser = z
   .record(z.string(), entityParser)
