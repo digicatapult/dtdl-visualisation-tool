e7cc362830cb347de00e18dcaf54dfbc20470150
David Blane <david.blane@digicatapult.org.uk>
2025-12-10 11:58:38 +0000

    fix: remove posthogMockServer from globalTeardown
    Removed references to posthogMockServer that was removed in earlier commit.
    This fixes the TypeScript compilation error.

--- test/globalTeardown.ts
+++ test/globalTeardown.ts
@@ -1,17 +1,10 @@
-import {
-  postgresContainer,
-  posthogMockServer,
-  visualisationUIContainer,
-  visualisationUIContainer2,
-} from './globalSetup'
-import { stopMockPostHogServer } from './mocks/posthogMock.js'
+import { postgresContainer, visualisationUIContainer, visualisationUIContainer2 } from './globalSetup'
 import { network } from './testcontainers/testContainersSetup'
 
 async function globalTeardown() {
   if (visualisationUIContainer) await visualisationUIContainer.stop()
   if (visualisationUIContainer2) await visualisationUIContainer2.stop()
   if (postgresContainer) await postgresContainer.stop()
-  if (posthogMockServer) await stopMockPostHogServer()
 
   await network.stop()
 }
