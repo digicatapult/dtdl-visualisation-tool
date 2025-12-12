f90777606dbfb0b2ec1e2c98100ab016e312cb00
Jonathan Gray <jonmattgray@gmail.com>
2025-12-12 16:22:05 +0000

    Complex schema edit (#444)
    * disable editing during req
    
    * disable more inputs
    
    * formatting
    
    * stop edit of complex schema
    
    * linting
    
    * linting
    
    * add undefined check

--- src/lib/server/models/strings.ts
+++ src/lib/server/models/strings.ts
@@ -19,7 +19,7 @@ export type DtdlId = string
  */
 export type UUID = string
 
-export const DTDL_VALID_SCHEMAS = [
+export const DTDL_PRIMITIVE_SCHEMAS = [
   'boolean',
   'byte',
   'bytes',
@@ -40,7 +40,13 @@ export const DTDL_VALID_SCHEMAS = [
   'unsignedShort',
   'uuid',
 ] as const
+export const DTDL_PRIMITIVE_SCHEMA_OPTIONS = DTDL_PRIMITIVE_SCHEMAS.map((opt) => ({ value: opt, label: opt }))
+export const DTDL_PRIMITIVE_SCHEMAS_SET = new Set(DTDL_PRIMITIVE_SCHEMAS.map((s) => s.toLowerCase()))
 
-export type DtdlSchema = (typeof DTDL_VALID_SCHEMAS)[number]
+export type DtdlSchema = (typeof DTDL_PRIMITIVE_SCHEMAS)[number]
 
-export const DTDL_VALID_WRITABLE = [true, false] as const
+const DTDL_VALID_WRITABLE = [true, false] as const
+export const DTDL_VALID_WRITABLE_OPTIONS = DTDL_VALID_WRITABLE.map((opt) => ({
+  value: String(opt),
+  label: String(opt),
+}))
